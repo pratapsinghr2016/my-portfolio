a133774f33e2f5287d89c00d05effbbc707af12c
Rajat Pratap Singh <rajat-1996_FE>
2022-12-18 04:32:00 +0530

    Create base for portfolio

---
+++ mail/contact.js
@@ -0,0 +1,74 @@
+$(function () {
+  $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function ($form, event, errors) {},
+    submitSuccess: function ($form, event) {
+      event.preventDefault();
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var subject = $("input#subject").val();
+      var message = $("textarea#message").val();
+      const serviceID = "service_y8jsc8q";
+      const templateID = "template_1eq5sjb";
+      const params = {
+        name,
+        email,
+        subject,
+        message,
+      };
+
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true);
+
+      emailjs
+        .send(serviceID, templateID, params)
+        .then(() => {
+          $("#success").html("<div class='alert alert-success'>");
+          $("#success > .alert-success")
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
+            .append("</button>");
+          $("#success > .alert-success").append(
+            "<strong>Your message has been sent. </strong>"
+          );
+          $("#success > .alert-success").append("</div>");
+          $("#contactForm").trigger("reset");
+        })
+        .catch(() => {
+          $("#success").html("<div class='alert alert-danger'>");
+          $("#success > .alert-danger")
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
+            .append("</button>");
+          $("#success > .alert-danger").append(
+            $("<strong>").text(
+              "Sorry " +
+                name +
+                ", it seems that our mail server is not responding. Please try again later!"
+            )
+          );
+          $("#success > .alert-danger").append("</div>");
+          $("#contactForm").trigger("reset");
+        })
+        .finally(() => {
+          setTimeout(function () {
+            $this.prop("disabled", false);
+          }, 1000);
+        });
+    },
+    filter: function () {
+      return $(this).is(":visible");
+    },
+  });
+
+  $('a[data-toggle="tab"]').click(function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+$("#name").focus(function () {
+  $("#success").html("");
+});
